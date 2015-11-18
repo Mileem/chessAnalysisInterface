c2c04fbb57bc0fce0185858f0baeec0d2f5be614
Sarah Ghidalia <Mileem@macbook-air-de-sarah.home>
2015-11-18 08:42:51 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chessAnalysisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chessAnalysisApp
+ */
+angular.module('chessAnalysisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
